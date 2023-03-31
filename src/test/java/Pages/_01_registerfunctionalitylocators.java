7530d9b52cb26adab0b1e4f6eeb656569d04cb37
yilmazas <yilmazahmetserdar@gmail.com>
2023-03-31 19:36:44 +0200

    third commit

--- src/test/java/Pages/_01_registerfunctionalitylocators.java
+++ src/test/java/Pages/_01_registerfunctionalitylocators.java
@@ -12,25 +12,54 @@ public class _01_registerfunctionalitylocators extends Parent {
     }
 
     @FindBy(linkText = "Register")
-    public WebElement registerButton;
+    public WebElement registerLink;
 
     @FindBy(id = "customer.firstName")
     public WebElement firstName;
-//
-//    @FindBy(id = "customer.lastName")
-//    public WebElement lastName;
-//
-//    @FindBy(id = "customer.address.street")
-//    public WebElement address;
 
-//    @FindBy(id = "customer.firstName")
-//    public WebElement firstName;
-//
-//    @FindBy(id = "customer.firstName")
-//    public WebElement firstName;
-//
-//    @FindBy(id = "customer.firstName")
-//    public WebElement firstName;
+    @FindBy(id = "customer.lastName")
+    public WebElement lastName;
+
+    @FindBy(id = "customer.address.street")
+    public WebElement address;
+
+    @FindBy(id = "customer.address.city")
+    public WebElement city;
+
+    @FindBy(id = "customer.address.state")
+    public WebElement state;
+
+    @FindBy(id = "customer.address.zipCode")
+    public WebElement zipCode;
+
+    @FindBy(id = "customer.phoneNumber")
+    public WebElement phone;
+
+    @FindBy(id = "customer.ssn")
+    public WebElement SSN;
+
+    @FindBy(id = "customer.username")
+    public WebElement userName;
+
+    @FindBy(id = "customer.password")
+    public WebElement password;
+
+    @FindBy(id = "repeatedPassword")
+    public WebElement confirm;
+
+    @FindBy(xpath = "(//input[@class='button'])[2]")
+    public WebElement registerButton;
+
+    @FindBy(xpath = "//p[text()='Your account was created successfully. You are now logged in.']")
+    public WebElement successMessage;
+
+
+
+
+
+
+
+
 
 
 //public WebElement getWebElement(String strButton){
--- src/test/java/StepDefinitions/_01_registerfunctionality.java
+++ src/test/java/StepDefinitions/_01_registerfunctionality.java
@@ -22,36 +22,52 @@ public class _01_registerfunctionality {
     @When("Create an account")
     public void createAnAccount() {
 
-        elements.registerButton.click();
-        Actions actions= new Actions(GWD.getDriver());
-        Action action= actions.click(elements.firstName)
-                .sendKeys("Hasan")
-                .sendKeys(Keys.TAB)
-                .sendKeys("Yildiz")
-                .sendKeys(Keys.TAB)
-                .sendKeys("cengaver caddesi")
-                .sendKeys(Keys.TAB)
-                .sendKeys("ankara")
-                .sendKeys(Keys.TAB)
-                .sendKeys("arizona")
-                .sendKeys(Keys.TAB)
-                .sendKeys("1234")
-                .sendKeys(Keys.TAB)
-                .sendKeys("05435678906")
-                .sendKeys(Keys.TAB)
-                .sendKeys("1237895")
-                .sendKeys(Keys.TAB)
-                .sendKeys("haskal")
-                .sendKeys(Keys.TAB)
-                .sendKeys("jhfr657sd")
-                .sendKeys(Keys.TAB)
-                .sendKeys("jhfr657sd")
-                .sendKeys(Keys.TAB).sendKeys(Keys.ENTER).build();
-                action.perform();
+       elements.clickFunction(elements.registerLink);
+       elements.sendKeysFunction(elements.firstName, "cihan");
+       elements.sendKeysFunction(elements.lastName, "yildiz");
+       elements.sendKeysFunction(elements.address, "akasya sokak ");
+       elements.sendKeysFunction(elements.city, "basel");
+       elements.sendKeysFunction(elements.state, "arizona");
+       elements.sendKeysFunction(elements.zipCode, "1234");
+       elements.sendKeysFunction(elements.phone, "05462347865");
+       elements.sendKeysFunction(elements.SSN, "5634");
+       elements.sendKeysFunction(elements.userName, "cihan101");
+       elements.sendKeysFunction(elements.password, "12345566");
+       elements.sendKeysFunction(elements.confirm, "12345566");
+
+       elements.clickFunction(elements.registerButton);
+
+//        Actions actions= new Actions(GWD.getDriver());
+//        Action action= actions.click(elements.firstName)
+//                .sendKeys("Hasan")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("Yildiz")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("cengaver caddesi")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("ankara")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("arizona")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("1234")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("05435678906")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("1237895")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("haskal")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("jhfr657sd")
+//                .sendKeys(Keys.TAB)
+//                .sendKeys("jhfr657sd")
+//                .sendKeys(Keys.TAB).sendKeys(Keys.ENTER).build();
+//                action.perform();
 
     }
 
     @Then("Success message should be displayed")
     public void successMessageShouldBeDisplayed() {
+
+        elements.verifyContainsTextFunction(elements.successMessage, "success");
     }
 }
