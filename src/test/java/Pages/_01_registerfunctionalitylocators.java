9c1783c9556317d021987a842a293aadcf285731
yilmazas <yilmazahmetserdar@gmail.com>
2023-03-31 18:53:18 +0200

    first commit

---
+++ src/test/java/Pages/_01_registerfunctionalitylocators.java
@@ -0,0 +1,24 @@
+package Pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class _01_registerfunctionalitylocators extends Parent{
+    public _01_registerfunctionalitylocators() {
+
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy()
+
+public WebElement getWebElement(String strButton){
+
+        switch (strButton){
+            case
+        }return null;
+
+    }
+}
+
---
+++ src/test/java/StepDefinitions/_01_registerfunctionality.java
@@ -0,0 +1,22 @@
+package StepDefinitions;
+
+import Pages._01_registerfunctionalitylocators;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_registerfunctionality {
+
+    _01_registerfunctionalitylocators elements= new _01_registerfunctionalitylocators();
+    @Given("Navigate to the ParaBank")
+    public void navigateToTheParaBank() {
+    }
+
+    @When("Create an account")
+    public void createAnAccount() {
+    }
+
+    @Then("Success message should be displayed")
+    public void successMessageShouldBeDisplayed() {
+    }
+}
